efd6f30bb087c551fb44791f0d09f8d19eb69862
AiriNarita <119647023+AiriNarita@users.noreply.github.com>
2023-01-14 09:08:21 +0000

    [add]4-13 destroy機能

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -20,6 +20,14 @@ class PostImagesController < ApplicationController
   end
 
 
+  def destroy
+    @post_image = PostImage.find(params[:id])
+    @post_image.destroy
+    redirect_to '/post_images'
+  end
+
+
+
 
    # 投稿データのストロングパラメータ
   private
