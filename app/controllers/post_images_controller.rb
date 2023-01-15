bcae86cf34e6ddff7c1ad3b12304915549ba8540
AiriNarita <119647023+AiriNarita@users.noreply.github.com>
2023-01-15 05:10:41 +0000

    [add]4-18 comment機能

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,4 +1,6 @@
 class PostImagesController < ApplicationController
+
+
   def new
      @post_image = PostImage.new
   end
@@ -17,6 +19,7 @@ class PostImagesController < ApplicationController
 
   def show
     @post_image = PostImage.find(params[:id])
+    @post_comment = PostComment.new
   end
 
 
